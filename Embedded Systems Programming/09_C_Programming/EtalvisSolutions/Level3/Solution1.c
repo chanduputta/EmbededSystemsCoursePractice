bbd5c7518fcd5dde8a61dfe6d78f3d0fadb57906
Shreyash Chandra <puttashreyash@gmial.com>
2025-03-07 19:46:12 +0530

    Etalvis

---
+++ Embedded Systems Programming/09_C_Programming/EtalvisSolutions/Level3/Solution1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main ()
+{
+	int User_input;
+	printf("Please enter an Integer :");
+	scanf("%d",&User_input);
+	if(User_input == 50)
+	{
+		printf("Success");
+	}
+	else
+	{
+		printf("Failure");
+	}
+}
+
+
+
